67829601cba4d94593e745deaf6cedaadc39760a
Douelfiqar <douidriss@gmail.com>
2023-04-27 15:05:58 +0100

    Long id

--- src/main/java/com/example/demo/entities/Document.java
+++ src/main/java/com/example/demo/entities/Document.java
@@ -17,7 +17,7 @@ public class Document  {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
 
-    private Integer id;
+    private Long id;
     private String sujet;
     private String titre;
     private String auteur;
