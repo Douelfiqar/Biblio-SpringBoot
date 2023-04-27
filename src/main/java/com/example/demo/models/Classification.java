a12b3f431b8d4ad960773ab3b609c526394d2149
Mohcine <malakchams006@gmail.com>
2023-04-27 14:20:55 +0100

    adding library and Classification classes

---
+++ src/main/java/com/example/demo/models/Classification.java
@@ -0,0 +1,16 @@
+package com.example.demo.models;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.ArrayList;
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Classification {
+        private ArrayList<String> sujets ;
+    private ArrayList<String> auteurs ;
+    private ArrayList<String> titres ;
+    private ArrayList<String> categories ;
+}
