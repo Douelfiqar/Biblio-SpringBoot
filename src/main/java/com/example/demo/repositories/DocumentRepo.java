b6d80513c30e88b8817592ab04fe60bf3a7e1e50
chadi <mzroc659@gmail.com>
2023-05-03 18:13:33 +0100

    added @Repository

--- src/main/java/com/example/demo/repositories/DocumentRepo.java
+++ src/main/java/com/example/demo/repositories/DocumentRepo.java
@@ -3,6 +3,8 @@ package com.example.demo.repositories;
 
 import com.example.demo.entities.Document;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface DocumentRepo extends JpaRepository<Document, Long> {
 }
